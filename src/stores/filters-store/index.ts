078b8887808648591828cdc01703ccfa19819ff8
Deolys <ed03aleks@mail.ru>
2024-09-15 22:53:11 +0300

    refactor: remove unnecessary code

--- src/stores/filters-store/index.ts
+++ src/stores/filters-store/index.ts
@@ -3,15 +3,6 @@ import { createEffect } from 'effector';
 import { usersApi } from '@/api/users-api';
 import { UsersFilters } from '@/types/user';
 
-import { $userTypes } from '../users-store';
-
 export const getFilteredUsersFx = createEffect(async (filters: UsersFilters) => {
-  const users = await usersApi.getFilteredUsers(filters);
-  const userTypes = $userTypes.getState();
-  const usersWithTypes = users.map((user) => {
-    const userType = userTypes.find((type) => type.id === user.type_id);
-    return { ...user, type: userType?.name };
-  });
-
-  return usersWithTypes;
+  return await usersApi.getFilteredUsers(filters);
 });
